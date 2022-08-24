dd83b1d1e11b7a301267394386b0ebf63781223e
Kalguri Mamatha <mamathasri629@gmail.com>
2022-08-24 20:02:44 +0530

    new changes

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -9,11 +9,10 @@ namespace StarChart.Controllers
 {
     [Route("")]
     [ApiController]
-    public class CelestialObjectControllers : ControllerBase
+    public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        
-        public CelestialObjectControllers(ApplicationDbContext context)
+        public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
             
